471279ce0a53980c79f2694f0e55352448df7ed2
Alex Ioannidis <a.ioannidis@cern.ch>
2024-10-26 12:51:45 +0200

    allow passing .xml files or .tar.gz

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use chrono::Utc;
 use flate2::read::GzDecoder;
 use std::fs;
-use std::io::{stdout, Read, Write};
+use std::io::{stdout, Read};
 use std::path::{Path, PathBuf};
 use std::{ffi::OsStr, fs::File};
 use tar::Archive;
@@ -10,9 +10,9 @@ use tar::Archive;
 use uuid::Uuid;
 
 use quick_xml::de::from_str;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct Identifier {
     #[serde(rename = "uri")]
     uri: String,
@@ -20,7 +20,7 @@ struct Identifier {
     path: String,
 }
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct PersonName {
     #[serde(rename = "given-names")]
     given_names: String,
@@ -28,12 +28,12 @@ struct PersonName {
     family_name: String,
 }
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct Person {
     name: PersonName,
 }
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct OrgIdentifier {
     #[serde(alias = "disambiguated-organization-identifier")]
     identifier: String,
@@ -41,38 +41,38 @@ struct OrgIdentifier {
     source: String,
 }
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct Organization {
     name: String,
     #[serde(alias = "disambiguated-organization")]
     identifier: OrgIdentifier,
 }
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct Employment {
     #[serde(alias = "end-date")]
     end: Option<()>,
     organization: Organization,
 }
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct AffiliationGroup {
     #[serde(alias = "employment-summary")]
     employment: Employment,
 }
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct Employments {
     #[serde(alias = "affiliation-group")]
     employment: Option<Vec<AffiliationGroup>>,
 }
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct Activities {
     employments: Employments,
 }
 
-#[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
+#[derive(Debug, PartialEq, Default, Deserialize)]
 struct Record {
     #[serde(alias = "orcid-identifier")]
     identifier: Identifier,
@@ -81,24 +81,25 @@ struct Record {
     activities: Activities,
 }
 
-#[derive(serde::Serialize)]
+#[derive(Debug, serde::Serialize)]
 struct NameIdentifier<'a> {
     scheme: &'a str,
     identifier: &'a str,
 }
 
-#[derive(serde::Serialize)]
+#[derive(Debug, serde::Serialize)]
 struct NameAffiliation<'a> {
     #[serde(skip_serializing_if = "Option::is_none")]
     id: Option<&'a str>,
     name: &'a str,
 }
 
-#[derive(serde::Serialize)]
+#[derive(Debug, serde::Serialize)]
 #[serde(tag = "$schema", rename = "local://names/name-v1.0.0.json")]
 struct NameJson<'a> {
     given_name: &'a str,
     family_name: &'a str,
+    name: &'a str,
     identifiers: Vec<NameIdentifier<'a>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     affiliations: Option<Vec<NameAffiliation<'a>>>,
@@ -147,6 +148,10 @@ fn csv_line_from_record(record: &Record) -> Result<Row> {
     let name_json = NameJson {
         given_name: &record.person.name.given_names,
         family_name: &record.person.name.family_name,
+        name: &format!(
+            "{}, {}",
+            record.person.name.family_name, record.person.name.given_names
+        ),
         identifiers: vec![NameIdentifier {
             scheme: "orcid",
             identifier: &record.identifier.path,
@@ -167,16 +172,14 @@ fn csv_line_from_record(record: &Record) -> Result<Row> {
     })
 }
 
-#[warn(dead_code)]
 fn parse_xml(xml_path: &Path) -> Result<Record> {
     let xml: String = fs::read_to_string(xml_path)?.parse()?;
     let record: Record = from_str(&xml)?;
-    writeln!(stdout(), "{}", serde_json::to_string(&record).unwrap())?;
     Ok(record)
 }
 
 fn parse_tgz(tgz_path: &Path) -> Result<()> {
-    let now = Utc::now().to_rfc3339();
+    // TODO: Probably return a stream of records instead of writing directly to stdout
     let mut writer = csv::WriterBuilder::new()
         .has_headers(false)
         .from_writer(stdout());
@@ -203,6 +206,15 @@ fn parse_tgz(tgz_path: &Path) -> Result<()> {
 }
 
 fn main() {
-    let tgz_path = std::env::args().nth(1).expect("No ORCiD dump path given.");
-    let _ = parse_tgz(&PathBuf::from(tgz_path));
+    let path = std::env::args().nth(1).expect("No ORCiD dump path given.");
+    if path.ends_with(".xml") {
+        let record = parse_xml(&PathBuf::from(path)).expect("Failed to parse XML");
+        let line = csv_line_from_record(&record).expect("Failed to convert to CSV");
+        let mut writer = csv::WriterBuilder::new()
+            .has_headers(false)
+            .from_writer(stdout());
+        writer.serialize(line).unwrap()
+    } else {
+        let _ = parse_tgz(&PathBuf::from(path));
+    }
 }
